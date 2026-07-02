9e9e60b2c357f9d05fccc711b3a946d022389cba
vitoriagdouradosouza008-a11y <vitoriagdouradosouza008@gmail.com>
2026-07-01 20:59:35 -0400

    arrumar

--- matriz/ex010/reserva.js
+++ matriz/ex010/reserva.js
@@ -1,34 +1,26 @@
-let poltrona = [];
+let reservaCinema = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+];
 
-for (let i = 0; i < 5; i++) {
-        poltrona[i] = [];
+function mapaCinema() {
+    const container = document.getElementById('mapa-cinema');
 
-    for (let j = 0; j < 5; j++) {
-        poltrona[i][j] = "Livre";
+    container.innerHTML = '';
+
+    if (poltrona.length === 0) {
+        let reserva = document(prompt("Digite a linha da poltrona que deseja reservar:"))
+        
     }
 }
 
-function reservaCinema() {
-    console.log("Mapa das poltronas:");
-    console.table(poltrona);
-
-    let linha = Number(prompt("Digite a linha da poltrona:"));
-    let coluna = Number(prompt("Digite a coluna da poltrona:"));
-
-    if (linha < 0 || coluna < 0) {
-        console.log("Reserva cancelada.");
-        return;
+function reservarPoltrona(linha, coluna) {
+    let poltrona = prompt("Digite 1 para reservar a poltrona ou 0 para liberar:");
+    if (poltrona === "0") {
+        reservaCinema[linha][coluna] = 0;
+    } else if (poltrona === "1") {
     }
-
-    if (linha >= 5 || coluna >= 5) {
-        console.log("Posição inválida! Digite valores entre 0 e 4.");
-    } else if (poltrona[linha][coluna] === "Livre") {
-        poltrona[linha][coluna] = "Ocupada";
-        console.log("Poltrona reservada com sucesso!");
-    } else {
-        console.log("Esse assento já está reservado!");
-    }
-
-    console.log("Mapa atualizado:");
-    console.log(poltrona);
 }
